dc685d7f0c322a97fc02a80ce6f66f43bd345c9f
Ibrahim Ammar <ibrahimammar931@gmail.com>
2026-08-06 10:50:05 +0100

    Delete taskflow directory

--- taskflow/tests/test_users.py
+++
@@ -1,9 +0,0 @@
-def test_get_me(client, auth_headers):
-    resp = client.get("/users/me", headers=auth_headers)
-    assert resp.status_code == 200
-    assert resp.json()["email"] == "test@example.com"
-
-
-def test_get_me_requires_auth(client):
-    resp = client.get("/users/me")
-    assert resp.status_code == 401
